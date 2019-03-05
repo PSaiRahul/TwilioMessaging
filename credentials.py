e7f8fd1a2a4eca9d9fe21198bfa2862cb44eb465
Sai Rahul <37973758+PSaiRahul@users.noreply.github.com>
2019-03-05 17:16:34 -0600

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = 'AC0984a6252d8ce854cffd6343d1e6cc02'
-auth_token = 'e8b29cb5f9a2924f30d880b6fdbb3382'
+account_sid = 'Give your account-sid which is created when signing up with Twilio'
+auth_token = 'Give your authentication-token which is created when signing up with Twilio'
 my_cell = 'Give your cell phone number with +code and no dashes or spaces'
 my_twilio = 'Give your created twilio number'
