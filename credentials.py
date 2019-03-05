973b2b7fb924581d47643545c2e7aca4bd0541a4
Sai Rahul <37973758+PSaiRahul@users.noreply.github.com>
2019-03-05 17:15:25 -0600

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 account_sid = 'AC0984a6252d8ce854cffd6343d1e6cc02'
 auth_token = 'e8b29cb5f9a2924f30d880b6fdbb3382'
-my_cell = '+16183537363'
-my_twilio = '+16182021034'
+my_cell = 'Give your cell phone number with +code and no dashes or spaces'
+my_twilio = 'Give your created twilio number'
